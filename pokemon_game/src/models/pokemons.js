02252ba9997e4a1fc5ad0979fef5f72760a91f91
Bry-13 <bryan.0702mc@gmail.com>
2024-12-10 11:11:36 -0600

    Todos los endpoints de Pokemons

--- pokemon_game/src/models/pokemons.js
+++ pokemon_game/src/models/pokemons.js
@@ -1,7 +1,11 @@
 const pokemonsModel = {
     getAll: 'SELECT * FROM pokemons',
-    
-
+    getById: 'SELECT * FROM pokemons WHERE id = ?',
+    getExisting: 'SELECT * FROM pokemons WHERE name = ? OR image = ?',
+    get3Random: 'SELECT * FROM pokemons ORDER BY RAND() LIMIT 3',
+    addPokemon: 'INSERT INTO pokemons (name, image) VALUES (?, ?)',
+    editPokemon: 'UPDATE pokemons SET name = COALESCE(?, name), image = COALESCE(?, image) WHERE id = ?',
+    pokemonvalid: 'SELECT * FROM pokemons WHERE (name = ? OR image = ?) AND id != ?',
+    eliminarPokemon: 'DELETE FROM pokemons WHERE id = ?',
 }
-
 module.exports = pokemonsModel;
